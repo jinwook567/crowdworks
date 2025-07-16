0e86f60a4a79dff1177d24a993330c049075db5a
이진욱 <dlwlsdnr567@naver.com>
2025-07-16 17:55:03 +0900

    feat: Document
    문서 렌더링 및 box라는 문서 위에 레이어를 두는 기능을 제공

---
+++ src/workspace/Document.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import DocumentComponent, {
+  type DocumentType,
+  type DocumentProps,
+} from "../documents/Document";
+import Frame from "./Frame";
+
+// top,right,bottom,left
+type Coord = [number, number, number, number];
+
+type Props = DocumentProps<DocumentType> & {
+  boxes: Coord[];
+  onBoxHoverEnter: (coord: Coord) => void;
+  onBoxHoverLeave: (coord: Coord) => void;
+  isSelected: (coord: Coord) => boolean;
+};
+
+function Document({
+  boxes,
+  onBoxHoverEnter,
+  onBoxHoverLeave,
+  isSelected,
+  ...props
+}: Props) {
+  return (
+    <Container>
+      <DocumentComponent {...props} />
+      {boxes.map((coord) => (
+        <Box
+          coord={coord}
+          onMouseEnter={() => onBoxHoverEnter(coord)}
+          onMouseLeave={() => onBoxHoverLeave(coord)}
+          shouldFocus={isSelected(coord)}
+          scrollPos="center"
+        />
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const Box = styled(Frame)<{ coord: Coord }>`
+  width: ${(props) => props.coord[1] - props.coord[3]}px;
+  height: ${(props) => props.coord[2] - props.coord[0]}px;
+  position: absolute;
+  top: ${(props) => props.coord[0]}px;
+  left: ${(props) => props.coord[3]}px;
+  opacity: 0.5;
+  ${(props) =>
+    props.shouldFocus &&
+    `
+    outline: 2px solid black;
+    background-color: yellow;
+    `}
+
+  &:hover {
+    outline: 2px solid black;
+    background-color: yellow;
+  }
+`;
+
+export default Document;
