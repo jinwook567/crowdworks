6258a5020511492bdea4504321837b4ab8e4d8e9
이진욱 <dlwlsdnr567@naver.com>
2025-07-16 22:10:10 +0900

    feat: Workspace App 컴포넌트에 할당

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import { type Props as WorkspaceProps } from "./workspace/Workspace";
+import Workspace from "./workspace/Workspace";
+import { parse } from "./parsers/json";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <></>;
+  const json = fetch("/1.report.json").then((response) => response.json());
+
+  const [data, setData] = useState<WorkspaceProps | null>(null);
+
+  useEffect(() => {
+    json.then((v) => setData(parse(v)));
+  }, []);
+
+  if (!data) return <div>fetching...</div>;
+  return <Workspace {...data} />;
 }
 
 export default App;
