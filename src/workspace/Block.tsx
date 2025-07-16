c5f46776ae5719e53db01b57be27d57be2dc4106
이진욱 <dlwlsdnr567@naver.com>
2025-07-16 17:51:15 +0900

    feat: Block
    문서를 파싱한 텍스트 블록을 렌더링하는 컴포넌트
    Container, Element를 동일한 방식으로 렌더링

---
+++ src/workspace/Block.tsx
@@ -0,0 +1,60 @@
+import Container, {
+  containerTypes,
+  type ContainerConfig,
+  type ContainerType,
+} from "../blocks/containers/Container";
+import Element, {
+  type ElementConfig,
+  type ElementType,
+} from "../blocks/elements/Element";
+import { type BaseConfig } from "../blocks/config";
+import Frame from "./Frame";
+import styled from "styled-components";
+
+type Props = (ContainerConfig<ContainerType> | ElementConfig<ElementType>) & {
+  onClick: (id: BaseConfig["id"]) => void;
+  isSelected: (id: BaseConfig["id"]) => boolean;
+};
+
+function Block({ onClick, isSelected, ...props }: Props) {
+  return (
+    <>
+      {isContainer(props) ? (
+        <Container {...props}>
+          {(elements) =>
+            elements.map((el, i) => (
+              <BlockFrame
+                onClick={() => onClick(props.elements[i].id)}
+                shouldFocus={isSelected(props.elements[i].id)}
+                scrollPos="nearest"
+              >
+                {el}
+              </BlockFrame>
+            ))
+          }
+        </Container>
+      ) : (
+        <BlockFrame
+          onClick={() => onClick(props.id)}
+          shouldFocus={isSelected(props.id)}
+          scrollPos="nearest"
+        >
+          <Element {...props} />
+        </BlockFrame>
+      )}
+    </>
+  );
+}
+
+const BlockFrame = styled(Frame)`
+  background: ${(props) =>
+    props.shouldFocus ? "rgba(255, 255, 0, 0.5)" : "transparent"};
+`;
+
+function isContainer(config: {
+  type: string;
+}): config is ContainerConfig<ContainerType> {
+  return containerTypes.some((type) => type === config.type);
+}
+
+export default Block;
